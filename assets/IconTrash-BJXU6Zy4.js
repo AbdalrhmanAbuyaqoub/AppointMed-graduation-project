24a4d2e84a53ed6e63644f95cf886d2f459bf673
abdalrhman <abdalrahmanabuyaqoub@gmail.com>
2025-06-25 17:41:33 +0300

    Updates

---
+++ assets/IconTrash-BJXU6Zy4.js
@@ -0,0 +1,11 @@
+import{b as a}from"./index-DFVxnAOD.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var h=a("outline","phone","IconPhone",[["path",{d:"M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2",key:"svg-0"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o=a("outline","trash","IconTrash",[["path",{d:"M4 7l16 0",key:"svg-0"}],["path",{d:"M10 11l0 6",key:"svg-1"}],["path",{d:"M14 11l0 6",key:"svg-2"}],["path",{d:"M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12",key:"svg-3"}],["path",{d:"M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3",key:"svg-4"}]]);export{o as I,h as a};
---
+++ assets/use-isomorphic-layout-effect.browser.esm-Dh_QfnL5.js
@@ -0,0 +1,1 @@
+import{r}from"./index-DFVxnAOD.js";var a=r.useLayoutEffect;export{a as i};
